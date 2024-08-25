d8cd33e0cb725d2b077248a4c9c31904de46e2cf
Martin-J-Larre <martinjlarre@gmail.com>
2024-08-24 23:26:55 -0400

    #1 Init

---
+++ src/arbolbinario/ArbolBinario.java
@@ -0,0 +1,19 @@
+//Nombre: 
+//Legajo: INFXXXXX
+//DNI: XXXXXXXX
+package arbolbinario;
+
+/**
+ *
+ * @author martin
+ */
+public class ArbolBinario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
